d361570c1423fe247c5f61744e88c21fa611f6be
Pablo <magazpablo@gmail.com>
2022-10-01 18:43:19 +0200

    chore: cleaning unnecesary deps

--- rs_algo_ws_server/src/main.rs
+++ rs_algo_ws_server/src/main.rs
@@ -1,23 +1,8 @@
-//! A chat server that broadcasts a message to all sessions.
-//!
-//! This is a simple line-based server which accepts WebSocket sessions,
-//! reads lines from those sessions, and broadcasts the lines to all other
-//! connected clients.
-//!
-//! You can test this out by running:
-//!
-//!     cargo run --example server 127.0.0.1:12345
-//!
-//! And then in another window run:
-//!
-//!     cargo run --example client ws://127.0.0.1:12345/
-//!
-//! You can run the second command in multiple windows and then chat between the
-//! two, seeing the messages from the other client as they're received. For all
-//! connected clients they'll all join the same room and see everyone else's
-//! messages.
-use std::io::Read;
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use dotenv::dotenv;
+use futures_channel::mpsc::{unbounded, UnboundedSender};
+use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
+use serde_json::Value;
+use std::time::Duration;
 use std::{
     collections::HashMap,
     env,
@@ -25,11 +10,6 @@ use std::{
     net::SocketAddr,
     sync::{Arc, Mutex},
 };
-use tokio::{pin, sync::mpsc, time::interval};
-use serde_json::Value;
-use dotenv::dotenv;
-use futures_channel::mpsc::{unbounded, UnboundedSender};
-use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
 
 use tokio::net::{TcpListener, TcpStream};
 use tungstenite::protocol::Message;
@@ -39,87 +19,87 @@ type Sessions = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
 
 use rs_algo_shared::broker::xtb::*;
 use rs_algo_shared::broker::*;
-use rs_algo_shared::helpers::date::{DateTime, Local, Utc};
 use tokio::time;
 
 fn handle_message(msg: Message, broker: &mut Xtb) -> String {
-     let data = match msg {
-                    Message::Ping(bytes) => {
-                        log::info!("ping received");
-                            "aaaa".to_string()
-                        //last_heartbeat = Instant::now();
-                        // unwrap:
-                        //session.pong(&bytes).await.unwrap();
-                    }
-                    Message::Pong(_) => {
-                        log::info!("pong received");
-                        "aaaa".to_string()
-                        //last_heartbeat = Instant::now();
-                    }
+    let data = match msg {
+        Message::Ping(bytes) => {
+            log::info!("ping received");
+            "".to_string()
+            //last_heartbeat = Instant::now();
+            // unwrap:
+            //session.pong(&bytes).await.unwrap();
+        }
+        Message::Pong(_) => {
+            log::info!("pong received");
+            "".to_string()
+            //last_heartbeat = Instant::now();
+        }
 
-                    Message::Text(msg) => {
-                        let msg: Value = serde_json::from_str(&msg).expect("Can't parse to JSON");
-                        let data = match msg["command"].clone() {
-                            Value::String(com) => match com.as_ref() {
-                                "subscribe" => {
-                                    let symbol = match &msg["arguments"]["symbol"] {
-                                        Value::String(s) => s,
-                                        _ => panic!("symbol parse error"),
-                                    };
-
-                                    let time_frame = match &msg["arguments"]["time_frame"] {
-                                        Value::String(s) => s,
-                                        _ => panic!("time_frame parse error"),
-                                    };
-
-                                    let strategy = match &msg["arguments"]["strategy"] {
-                                        Value::String(s) => s,
-                                        _ => panic!("strategy parse error"),
-                                    };
-
-                                    let strategy_type = match &msg["arguments"]["strategy_type"] {
-                                        Value::String(s) => s,
-                                        _ => panic!("strategy type parse error"),
-                                    };
-
-                                    let name = &[symbol, "_", time_frame].concat();
-                                    let client_name = &[strategy, "_", strategy_type].concat();
-                                    
-                                    println!("99999999 {} {}", name, client_name);
-                                    let res = broker.get_instrument_data2(symbol,1440,1656109158).unwrap();
-                                    serde_json::to_string(&res).unwrap()
-                                    //self.symbol_subscribe(name, client_name, ctx);
-                                }
-                                &_ => {
-                                    log::error!("unknown command received {}", com);
-                                    "aaaa".to_string()
-                                }
-                            },
-                            _ => {
-                                    log::error!("Wrong command format");
-                                    "aaaa".to_string()
-                                }
+        Message::Text(msg) => {
+            let msg: Value = serde_json::from_str(&msg).expect("Can't parse to JSON");
+            let data = match msg["command"].clone() {
+                Value::String(com) => match com.as_ref() {
+                    "subscribe" => {
+                        let symbol = match &msg["arguments"]["symbol"] {
+                            Value::String(s) => s,
+                            _ => panic!("symbol parse error"),
                         };
 
-                        data
-                    }
-                    //  Message::Close(msg) => {
-                    //     println!("Disconected {:?}", msg);
-                    // }
+                        let time_frame = match &msg["arguments"]["time_frame"] {
+                            Value::String(s) => s,
+                            _ => panic!("time_frame parse error"),
+                        };
 
-                     _ => {
-                         println!("Error {:?}", msg);
-                        "aaaa".to_string()
+                        let strategy = match &msg["arguments"]["strategy"] {
+                            Value::String(s) => s,
+                            _ => panic!("strategy parse error"),
+                        };
+
+                        let strategy_type = match &msg["arguments"]["strategy_type"] {
+                            Value::String(s) => s,
+                            _ => panic!("strategy type parse error"),
+                        };
+
+                        let name = &[symbol, "_", time_frame].concat();
+                        let client_name = &[strategy, "_", strategy_type].concat();
+
+                        println!("99999999 {} {}", name, client_name);
+                        let res = broker
+                            .get_instrument_data2(symbol, 1440, 1656109158)
+                            .unwrap();
+                        serde_json::to_string(&res).unwrap()
+                        //self.symbol_subscribe(name, client_name, ctx);
                     }
+                    &_ => {
+                        log::error!("unknown command received {}", com);
+                        "".to_string()
+                    }
+                },
+                _ => {
+                    log::error!("Wrong command format");
+                    "".to_string()
+                }
             };
+
             data
+        }
+        //  Message::Close(msg) => {
+        //     println!("Disconected {:?}", msg);
+        // }
+        _ => {
+            println!("Error {:?}", msg);
+            "".to_string()
+        }
+    };
+    data
 }
 
-fn send_message(sessions: Sessions, addr: SocketAddr, msg: Message) {
+fn send_message(sessions: Sessions, addr: &SocketAddr, msg: Message) {
     let recipients = sessions.lock().unwrap();
-    let recipient = match recipients.get(&addr) {
+    let recipient = match recipients.get(addr) {
         Some(recp) => recp.unbounded_send(msg).unwrap(),
-        None => panic!("recipient not found!")
+        None => panic!("recipient not found!"),
     };
 }
 
@@ -135,34 +115,42 @@ fn broadcast_message(sessions: Sessions, addr: SocketAddr, msg: Message) {
     }
 }
 
+async fn init_heart_beat(sessions: Sessions, addr: SocketAddr) {
+    let sessions = sessions.clone();
+    tokio::spawn(async move {
+        let hb_interval = env::var("HEARTBEAT_INTERVAL")
+            .unwrap()
+            .parse::<u64>()
+            .unwrap();
 
-async fn handle_connection(
-    sessions: Sessions,
-    raw_stream: &mut TcpStream,
-    addr: SocketAddr,
-) {
+        let hb_client_timeout = env::var("HB_CLIENT_TIMEOUT")
+            .unwrap()
+            .parse::<u64>()
+            .unwrap();
+        let mut interval = time::interval(Duration::from_millis(hb_interval));
+        loop {
+            interval.tick().await;
+            send_message(
+                sessions.clone(),
+                &addr,
+                Message::Ping("".as_bytes().to_vec()),
+            );
+        }
+    });
+}
+
+async fn handle_session(sessions: Sessions, raw_stream: &mut TcpStream, addr: SocketAddr) {
     println!("Incoming TCP connection from: {}", addr);
-
     let username = &env::var("BROKER_USERNAME").unwrap();
     let password = &env::var("BROKER_PASSWORD").unwrap();
     let mut broker = Xtb::new().await;
     broker.login(username, password).await.unwrap();
-    //broker.get_symbols2();
-
-    // let hb_interval = Duration::from_secs(env::var("HEARTBEAT_INTERVAL")
-    //     .unwrap()
-    //     .parse::<u64>()
-    //     .unwrap());
-    // let mut interval = interval(hb_interval);
-
-
+    init_heart_beat(sessions.clone(), addr).await;
 
     loop {
         let ws_stream = tokio_tungstenite::accept_async(&mut *raw_stream)
             .await
             .expect("Error during the websocket handshake occurred");
-        
-            println!("WebSocket connection established: {}", addr);
 
         // Insert the write part of this peer to the peer map.
         let (tx, rx) = unbounded();
@@ -170,12 +158,13 @@ async fn handle_connection(
 
         let (outgoing, incoming) = ws_stream.split();
         let broadcast_incoming = incoming.try_for_each(|msg| {
-            log::info!("MSG received from {}: {}", addr,msg.to_text().unwrap());
+            println!("MSG received from {}: {}", addr, msg.to_text().unwrap());
 
             let data = handle_message(msg, &mut broker);
-            let message = Message::Text(data.to_string());
+            if data != "" {
+                send_message(sessions.clone(), &addr, Message::Text(data))
+            };
 
-            send_message(sessions.clone(),addr,message);
             future::ok(())
         });
 
@@ -202,7 +191,7 @@ async fn main() -> Result<(), IoError> {
     while let Ok((mut stream, addr)) = socket.accept().await {
         let sessions = sessions.clone();
         tokio::spawn(async move {
-            handle_connection(sessions.clone(), &mut stream, addr).await;
+            handle_session(sessions.clone(), &mut stream, addr).await;
         });
     }
 
