9278faff794eff761f6e3bfee2ff3bec53506677
Pablo <magazpablo@gmail.com>
2022-12-02 13:31:27 +0100

    chore: update wrong package path

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,12 @@ do
     case $opt in
         "build & deploy all")
             echo "Deploying: $opt";
-            docker build -t cluster.loc:5000/rs-algo-ws-server:latest rs_algo_ws_server ; docker push cluster.loc:5000/rs-algo-ws-server:latest ; ansible-playbook playbook.yml
+            docker build -t cluster.loc:5000/rs-algo-ws-server:latest rs_algo_ws_server ; docker push cluster.loc:5000/rs-algo-ws-server:latest ; docker build -t cluster.loc:5000/rs-algo-bot:latest rs_algo_bot ; docker push cluster.loc:5000/rs-algo-bot:latest ; ansible-playbook playbook.yml
             break
             ;;
         "build all")
             echo "Deploying: $opt";
-            docker build -t cluster.loc:5000/rs-algo-ws-server:latest rs_algo_ws_server ;
+            docker build -t cluster.loc:5000/rs-algo-ws-server:latest rs_algo_ws_server ; docker build -t cluster.loc:5000/rs-algo-bot:latest rs_algo_bot
             break
             ;;
         "deploy all")
@@ -23,6 +23,11 @@ do
         "build & deploy rs-algo-ws-server")
             echo "Deploying: $opt";
             docker build -t cluster.loc:5000/rs-algo-ws-server:latest rs_algo_ws_server ; docker push cluster.loc:5000/rs-algo-ws-server:latest ; ansible-playbook playbook.yml
+            break
+            ;;
+        "build & deploy rs-algo-bot")
+            echo "Deploying: $opt";
+            docker build -t cluster.loc:5000/rs-algo-bot:latest rs_algo_bot ; docker push cluster.loc:5000/rs-algo-bot:latest
             break
             ;;
         "Quit")
