86160b555b28bf28fa5a762435c94c6b8fd48bca
Pablo <magazpablo@gmail.com>
2024-01-13 20:00:14 +0100

    ops: new test

--- rs_algo_bot/src/strategies/strategy.rs
+++ rs_algo_bot/src/strategies/strategy.rs
@@ -487,7 +487,7 @@ pub fn set_strategy(
         ),
         Box::new(
             strategies::bollinger_bands_reversals_buy_exit::BollingerBandsReversals::new(
-                Some("BB_Reversals_Backtest_H4513"),
+                Some("BB_Reversals_Backtest_H4813"),
                 Some(time_frame),
                 higher_time_frame,
                 Some(strategy_type.clone()),
