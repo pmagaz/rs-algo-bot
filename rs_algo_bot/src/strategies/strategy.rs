dc09c6aa568e2358251728ac67db38d5d8b9d377
Pablo <magazpablo@gmail.com>
2024-01-13 14:39:45 +0100

    ops: new test

--- rs_algo_bot/src/strategies/strategy.rs
+++ rs_algo_bot/src/strategies/strategy.rs
@@ -478,7 +478,7 @@ pub fn set_strategy(
     let strategies: Vec<Box<dyn Strategy>> = vec![
         Box::new(
             strategies::bollinger_bands_reversals_buy_exit::BollingerBandsReversals::new(
-                Some("BB_Reversals_Backtest"),
+                Some("BB_Reversals_Backtest_813"),
                 Some(time_frame),
                 higher_time_frame,
                 Some(strategy_type.clone()),
@@ -487,7 +487,7 @@ pub fn set_strategy(
         ),
         Box::new(
             strategies::bollinger_bands_reversals_buy_exit::BollingerBandsReversals::new(
-                Some("BB_Reversals_Backtest_H4"),
+                Some("BB_Reversals_Backtest_H4513"),
                 Some(time_frame),
                 higher_time_frame,
                 Some(strategy_type.clone()),
